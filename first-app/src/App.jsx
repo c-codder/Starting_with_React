a7f578629f6e4690bdd3103ee2edba2778deb357
e <e2205684@edu.vamk.fi>
2025-03-19 17:34:55 +0200

    Combined all elements into App.jsx and completed the React application.

--- first-app/src/App.jsx
+++ first-app/src/App.jsx
@@ -1,14 +1,36 @@
 import React from "react";
 import Greeting from "./components/Greeting.jsx";
+import ThemeToggle from "./components/ThemeToggle.jsx";
+import LoginMessage from "./components/LoginMessage.jsx";
+import TodoList from "./components/TodoList.jsx";
+import './index.css';
 
 function App() {
+    const tasks = ["Eat", "Study", "Walk", "Read", "Relax", "Sleep"];
+
     return (
         <div>
-            <Greeting name="Stanislav" />
+            {/* Greeting in a red frame */}
+            <div className="red-frame">
+                <Greeting name="Stanislav" />
+            </div><br></br>
+
+            {/* Theme toggle (without a red frame) */}
             <ThemeToggle />
-            <LoginMessage isLoggedIn={true} />
+
+            {/* Login message in a red frame */}
+            <div className="red-frame">
+                <LoginMessage isLoggedIn={true} />
+            </div><br></br>
+
+            {/* My tasks for today in a red frame */}
+            <div className="red-frame">
+                <h2>My tasks for today</h2>
+                <TodoList todos={tasks} />
+            </div>
         </div>
     );
 }
 
 export default App;
+
---
+++ first-app/src/components/TodoList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function TodoList({ todos }) {
+    return (
+        <div>
+            <ul>
+                {todos.map((todo, index) => (
+                    <li key={index}>{todo}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}

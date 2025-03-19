ccb2194f396ba60f7e47b36036e627580329603b
e <e2205684@edu.vamk.fi>
2025-03-19 16:00:41 +0200

    Initial commit with React project files

---
+++ first-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Greeting from "./components/Greeting.jsx";
+
+function App() {
+    return (
+        <div>
+            <Greeting name="Stanislav" />
+        </div>
+    );
+}
+
+export default App;
